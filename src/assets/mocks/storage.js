21cfaf061756d0efa93bfe234bb1bbed972b1755
Michael Riedel <meeque@meeque.de>
2024-12-22 15:57:41 +0100

    Add simple mock page that displays browser storage contents.

---
+++ src/assets/mocks/storage.js
@@ -0,0 +1,33 @@
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        const storageViewTemplate = document.getElementById('storageView').content;
+        const storageViewEntryTemplate = document.getElementById('storageViewEntry').content;
+
+        function storageViewController(containerElement, storage) {
+
+            const storageView = storageViewTemplate.cloneNode(true).querySelector('table');
+
+            for (let i = 0; i < storage.length; i++) {
+                const key = storage.key(i);
+                const item = storage.getItem(key);
+
+                const storageViewEntry = storageViewEntryTemplate.cloneNode(true).querySelector('tr');
+                storageViewEntry.querySelector('.index').textContent = i;
+                storageViewEntry.querySelector('.key').textContent = key;
+                storageViewEntry.querySelector('.item').textContent = item;
+
+                storageView.insertAdjacentElement('beforeend', storageViewEntry);
+            }
+            
+            containerElement.insertAdjacentElement('beforeend', storageView);
+        }
+
+        for (const localStorageViewContainer of document.querySelectorAll('.localStorage')) {
+            storageViewController(localStorageViewContainer, window.localStorage);
+        }
+        for (const sessionStorageViewContainer of document.querySelectorAll('.sessionStorage')) {
+            storageViewController(sessionStorageViewContainer, window.sessionStorage);
+        }
+    }
+);
