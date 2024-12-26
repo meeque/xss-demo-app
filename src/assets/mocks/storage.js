afba5834a2beaece726306cd7b912a39b516e30b
Michael Riedel <meeque@meeque.de>
2024-12-26 15:04:42 +0100

    Automatically update storage mock on changes.
    Also refactor mock templates.

--- src/assets/mocks/storage.js
+++ src/assets/mocks/storage.js
@@ -4,35 +4,61 @@ document.addEventListener(
         const storageViewTemplate = document.getElementById('storageView').content;
         const storageViewEntryTemplate = document.getElementById('storageViewEntry').content;
 
+        function remove(node) {
+            if (node && node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+        }
+
         function storageViewController(containerElement, storage) {
 
+            remove(containerElement.querySelector('div.storageView'));
+
+            const storageView = storageViewTemplate.cloneNode(true).querySelector('div.storageView');
+            const storageViewTable = storageView.querySelector('table');
+            const storageViewMessageEmpty = storageView.querySelector('p.messageEmpty');
+
             if (storage.length == 0) {
-                containerElement.insertAdjacentText('beforeend', 'No items in this storage.');
-                return;
-            }
 
-            const storageView = storageViewTemplate.cloneNode(true).querySelector('table');
+                remove(storageViewTable);
 
-            for (let i = 0; i < storage.length; i++) {
-                const key = storage.key(i);
-                const item = storage.getItem(key);
+            } else {
 
-                const storageViewEntry = storageViewEntryTemplate.cloneNode(true).querySelector('tr');
-                storageViewEntry.querySelector('.index').textContent = i;
-                storageViewEntry.querySelector('.key').textContent = key;
-                storageViewEntry.querySelector('.item').textContent = item;
+                remove(storageViewMessageEmpty);
 
-                storageView.insertAdjacentElement('beforeend', storageViewEntry);
+                for (let i = 0; i < storage.length; i++) {
+                    const key = storage.key(i);
+                    const item = storage.getItem(key);
+
+                    const storageViewEntry = storageViewEntryTemplate.cloneNode(true).querySelector('tr');
+                    storageViewEntry.querySelector('.index').textContent = i;
+                    storageViewEntry.querySelector('.key').textContent = key;
+                    storageViewEntry.querySelector('.item').textContent = item;
+
+                    storageViewTable.insertAdjacentElement('beforeend', storageViewEntry);
+                }
+
             }
             
             containerElement.insertAdjacentElement('beforeend', storageView);
         }
 
-        for (const localStorageViewContainer of document.querySelectorAll('.localStorage')) {
-            storageViewController(localStorageViewContainer, window.localStorage);
+        function initStorageViews() {
+            for (const localStorageViewContainer of document.querySelectorAll('.localStorage')) {
+                storageViewController(localStorageViewContainer, window.localStorage);
+            }
+            for (const sessionStorageViewContainer of document.querySelectorAll('.sessionStorage')) {
+                storageViewController(sessionStorageViewContainer, window.sessionStorage);
+            }
         }
-        for (const sessionStorageViewContainer of document.querySelectorAll('.sessionStorage')) {
-            storageViewController(sessionStorageViewContainer, window.sessionStorage);
-        }
+
+        initStorageViews();
+
+        window.addEventListener(
+            'storage',
+            (event) => {
+                initStorageViews();
+            }
+        );
     }
 );
