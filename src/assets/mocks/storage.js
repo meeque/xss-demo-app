02a07364518ac0b3055cc8ac7525b9dd29972658
Michael Riedel <meeque@meeque.de>
2024-12-27 01:05:32 +0100

    Add support for adding storage items in the storage mock.

--- src/assets/mocks/storage.js
+++ src/assets/mocks/storage.js
@@ -4,6 +4,7 @@ document.addEventListener(
         const storageViewTemplate = document.getElementById('storageView').content;
         const storageViewEntryTemplate = document.getElementById('storageViewEntry').content;
         const storageViewEntryEditTemplate = document.getElementById('storageViewEntryEdit').content;
+        const storageViewEntryNewTemplate = document.getElementById('storageViewEntryNew').content;
 
         function remove(node) {
             if (node && node.parentNode) {
@@ -31,39 +32,60 @@ document.addEventListener(
                 'click',
                 () => {
                     storage.removeItem(key);
-                    remove(storageViewEntry);
+                    initStorageViews();
                 }
             );
 
-            containerElement.insertAdjacentElement('beforeend', storageViewEntry);
+            containerElement.querySelector('tr.actions').insertAdjacentElement('beforebegin', storageViewEntry);
         }
 
         function storageViewEntryEditController(storageViewEntryElement, storage, index) {
             const key = storage.key(index)
             const item = storage.getItem(key);
 
-            const storageViewEditEntry = storageViewEntryEditTemplate.cloneNode(true).querySelector('tr');
-            storageViewEditEntry.querySelector('.index').textContent = index;
-            storageViewEditEntry.querySelector('.key').textContent = key;
-            storageViewEditEntry.querySelector('.item input').value = item;
+            const storageViewEntryEdit = storageViewEntryEditTemplate.cloneNode(true).querySelector('tr');
+            storageViewEntryEdit.querySelector('.index').textContent = index;
+            storageViewEntryEdit.querySelector('.key').textContent = key;
+            storageViewEntryEdit.querySelector('.item input').value = item;
 
-            storageViewEntryElement.insertAdjacentElement('afterend', storageViewEditEntry);
+            storageViewEntryEdit.querySelector('.actions button[name=save]').addEventListener(
+                'click',
+                () => {
+                    storage.setItem(key, storageViewEntryEdit.querySelector('.item input').value);
+                    initStorageViews();
+                }
+            );
+
+            storageViewEntryEdit.querySelector('.actions button[name=cancel]').addEventListener(
+                'click',
+                () => {
+                    initStorageViews();
+                }
+            );
+
+            storageViewEntryElement.insertAdjacentElement('afterend', storageViewEntryEdit);
             remove(storageViewEntryElement);
+        }
 
-            storageViewEditEntry.querySelector('.actions button[name=save]').addEventListener(
+        function storageViewEntryNewController(containerElement, storage) {
+            const storageViewEntryNew = storageViewEntryNewTemplate.cloneNode(true).querySelector('tr');
+
+            storageViewEntryNew.querySelector('.actions button[name=save]').addEventListener(
                 'click',
                 () => {
-                    storage.setItem(key, storageViewEditEntry.querySelector('.item input').value);
+                    storage.setItem(storageViewEntryNew.querySelector('.key input').value, storageViewEntryNew.querySelector('.item input').value);
                     initStorageViews();
                 }
             );
 
-            storageViewEditEntry.querySelector('.actions button[name=cancel]').addEventListener(
+            storageViewEntryNew.querySelector('.actions button[name=cancel]').addEventListener(
                 'click',
                 () => {
                     initStorageViews();
                 }
             );
+
+            containerElement.querySelector('tr.actions').insertAdjacentElement('beforebegin', storageViewEntryNew);
         }
 
         function storageViewController(containerElement, storage) {
@@ -72,21 +94,22 @@ document.addEventListener(
 
             const storageView = storageViewTemplate.cloneNode(true).querySelector('div.storageView');
             const storageViewTable = storageView.querySelector('table');
-            const storageViewMessageEmpty = storageView.querySelector('p.messageEmpty');
+            const storageViewMessageEmpty = storageView.querySelector('tr.messageEmpty');
+            const storageViewButtonNew = storageView.querySelector('tr.actions button[name=new]');
 
-            if (storage.length == 0) {
-
-                remove(storageViewTable);
-
-            } else {
-
+            if (storage.length != 0) {
                 remove(storageViewMessageEmpty);
-
                 for (let i = 0; i < storage.length; i++) {
                     storageViewEntryController(storageViewTable, storage, i);
                 }
-
             }
+
+            storageViewButtonNew.addEventListener(
+                'click',
+                () => {
+                    storageViewEntryNewController(storageViewTable, storage);
+                }
+            );
 
             containerElement.insertAdjacentElement('beforeend', storageView);
         }
