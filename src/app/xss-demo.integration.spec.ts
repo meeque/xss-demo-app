4a67e4700c08f466c6508d5b7fd86e9ccc7f693c
Michael Riedel <meeque@meeque.de>
2025-02-14 22:12:48 +0100

    Start making integration tests for payload outputs more generic.
    Test payload outputs against suitable presets.
    Work in progress, some of these new test are still failing.

--- src/app/xss-demo.integration.spec.ts
+++ src/app/xss-demo.integration.spec.ts
@@ -1,27 +1,28 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { xssDemoConfig } from './xss-demo.config';
 import { XssDemoComponent } from './xss-demo.component';
+import { PayloadOutputContext, PayloadOutputService } from './payload-output.service';
 
-describe('Xss Demo App', () => {
+describe('Xss Demo App', async () => {
+
+  let payloadOutputService = new PayloadOutputService(null);
 
   let fixture : ComponentFixture<XssDemoComponent>;
   let component : XssDemoComponent;
   let element : HTMLElement;
 
-  let payloadInputComboBox;
-  let payloadOutputComboBox;
-  let alertOverlay;
+  let payloadInputComboBox : HTMLElement;
+  let payloadOutputComboBox : HTMLElement;
+  let alertOverlay : HTMLElement;
+
+  let xssResolve = (value: any) => {};
 
   function timeout(millis: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, millis));
   }
 
-  function spyOnAndCallThroughAndPromise(object: any, method: string): Promise<any> {
-    let spyCallResolve: (value: any) => void;
-    const spyInvokePromise = new Promise<any>((resolve) => spyCallResolve = resolve );
-    const methodImpl = object[method];
-    spyOn(object, method).and.callFake((... invokeArgs) => spyCallResolve(methodImpl(... invokeArgs)));
-    return spyInvokePromise;
+  function nextXssPromise(): Promise<any> {
+    return new Promise<any>((resolve) => xssResolve = resolve );
   }
 
   async function select(input: string, output: string) {
@@ -37,7 +38,7 @@ describe('Xss Demo App', () => {
     fixture.detectChanges();
   }
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     TestBed.configureTestingModule(xssDemoConfig);
     await TestBed.compileComponents();
     fixture = TestBed.createComponent(XssDemoComponent);
@@ -48,17 +49,68 @@ describe('Xss Demo App', () => {
 
     payloadInputComboBox = element.querySelector('section.input-area combobox-input');
     payloadOutputComboBox = element.querySelector('section.output-area combobox-input');
-    alertOverlay = element.querySelector('.fd-shell__overlay.fd-overlay--alert')
+    alertOverlay = element.querySelector('.fd-shell__overlay.fd-overlay--alert');
+
+    const xssOriginal = window['xss'];
+    spyOn(window as any, 'xss').and.callFake(() => {
+      xssOriginal();
+      xssResolve(null);
+    });
   });
 
-  it('should be created', () => {
+  it('should be created', async () => {
     expect(component).toBeDefined();
   });
 
-  it('with image onerror input and raw HTML output should trigger XSS', async () => {
-    const xssPromise = spyOnAndCallThroughAndPromise(window, 'xss');
-    await select('Image onerror', 'Raw HTML');
-    await Promise.race([xssPromise, timeout(1000)]);
-    expect(alertOverlay.querySelector('.alert-xss-triggered')).not.toBeNull();
-  });
+  const xssTriggeringPresetsByContextAndOutput = {};
+  xssTriggeringPresetsByContextAndOutput[PayloadOutputContext.HtmlContent.toString()] = {
+    "HtmlContent" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "DomInnerHtml" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "DomInnerHtmlNoOutput" : ["Image onerror"],
+    "JQueryHtml" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryConstructor" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryPrepend" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryAppend" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryBefore" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryAfter" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryWrapInner" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryWrap" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "JQueryReplaceWith" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+    "NgTrusted" : ["IFrame src", "Image onerror", "Image onerror (legacy flavors)", "Mixed HTML Content"],
+  }
+
+  for (const contextDescriptor of payloadOutputService.descriptors) {
+
+    // XXX ignore contexts that have not been configured yet
+    if (!Object.hasOwn(xssTriggeringPresetsByContextAndOutput, contextDescriptor.id.toString())) {
+      continue;
+    }
+
+    describe('in the context of ' + contextDescriptor.name, () => {
+      for (const outputDescriptor of contextDescriptor.payloadOutputs) {
+
+        describe('with payload output ' + outputDescriptor.name, () => {
+
+          it('should trigger XSS for appropriate presets', async () => {
+            const matchingPresetGroup = component.presetGroups.find(menuGroup => menuGroup.value == contextDescriptor.id);
+
+            for (const matchingPreset of matchingPresetGroup.items) {
+              const xssPromise = nextXssPromise();
+              await select(matchingPreset.name, outputDescriptor.name);
+              const timeoutPromise = timeout(500);
+              await Promise.race([xssPromise, timeoutPromise]);
+
+              if (matchingPreset.name in (xssTriggeringPresetsByContextAndOutput[contextDescriptor.id.toString()][outputDescriptor.id] || [])) {
+                await expectAsync(xssPromise).withContext("preset " + matchingPreset.name + " should trigger XSS").already.toBeResolved();
+                expect(alertOverlay.querySelector('.alert-xss-triggered')).withContext("preset " + matchingPreset.name + " should show XSS alert message").not.toBeNull();
+              } else {
+                await expectAsync(timeoutPromise).withContext("preset " + matchingPreset.name + " should NOT trigger XSS").already.toBeResolved();
+                expect(alertOverlay.querySelector('.alert-xss-triggered')).withContext("preset " + matchingPreset.name + " should NOT show XSS alert message").toBeNull();
+              }
+            }
+          }, 30000);
+        });
+      }
+    });
+  }
 });
