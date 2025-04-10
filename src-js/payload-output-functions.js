8980714bb2768b6af535ce2197b5cb3277d7c1b8
Michael Riedel <meeque@meeque.de>
2025-04-10 22:25:53 +0200

    Clean up trailing white-space.

--- src-js/payload-output-functions.js
+++ src-js/payload-output-functions.js
@@ -6,31 +6,31 @@
     PayloadOutputFunctions: {
 
       PayloadProcessors: class PayloadProcessors {
-      
+
         constructor(sanitizer) {
           this.sanitizer = sanitizer;
         }
-      
+
         ngTrustedHtml = (payload) => {
           return this.sanitizer.bypassSecurityTrustHtml(payload);
         }
-      
+
         ngTrustedUrl = (payload) => {
           return this.sanitizer.bypassSecurityTrustUrl(payload);
         }
-      
+
         ngTrustedResourceUrl = (payload) => {
           return this.sanitizer.bypassSecurityTrustResourceUrl(payload);
         }
-      
+
         ngTrustedStyle(payload) {
           return this.sanitizer.bypassSecurityTrustStyle(payload);
         }
-      
+
         domTextNode(payload) {
           return document.createTextNode(payload);
         }
-      
+
         htmlEncoding(payload) {
           return payload
               .replace(/&/g, '&amp;')
@@ -39,11 +39,11 @@
               .replace(/"/g, '&quot;')
               .replace(/'/g, '&#x27;');
         }
-      
+
         htmlSanitizingDomPurify(payload) {
           return DOMPurify.sanitize(payload);
         }
-      
+
         htmlSanitizingDomPurifyMinimalInline(payload) {
           return DOMPurify.sanitize(
             payload,
@@ -52,7 +52,7 @@
               ALLOWED_ATTR: ['class']
             });
         }
-      
+
         htmlSanitizingDomPurifyInlineBlockLinks(payload) {
           return DOMPurify.sanitize(
             payload,
@@ -61,7 +61,7 @@
               ALLOWED_ATTR: ['class', 'href', 'target']
             });
         }
-      
+
         urlValidation(payload) {
           try {
             var url = new URL(payload, document.baseURI);
@@ -73,7 +73,7 @@
           }
           return payload;
         }
-      
+
         jsonParsing(payload) {
           try {
             return JSON.parse(payload);
@@ -82,19 +82,19 @@
             return {};
           }
         }
-      
+
         jsEncoding(payload) {
           return 'var outputElement = document.createElement(\'div\');\n'
               + 'outputElement.textContent = ' + JSON.stringify(payload) + ';\n'
               + 'document.currentScript.insertAdjacentElement(\'afterend\', outputElement);';
         }
-      
+
         jsDoubleQuoting(payload) {
           return 'var outputElement = document.createElement(\'div\');\n'
               + 'outputElement.textContent = "' + payload + '";\n'
               + 'document.currentScript.insertAdjacentElement(\'afterend\', outputElement);';
         }
-      
+
         jsSingleQuoting(payload) {
           return 'var outputElement = document.createElement(\'div\');\n'
               + 'outputElement.textContent = \'' + payload + '\';\n'
@@ -122,60 +122,60 @@
         textContent(element, payload) {
           element.textContent = payload;
         }
-      
+
         innerText(element, payload) {
           element.innerText = payload;
         }
-      
+
         innerHtml(element, payload) {
           element.innerHTML = payload;
         }
-      
+
         innerHtmlNoOutput(element, payload) {
           document.createElement('div').innerHTML = payload;
         }
-      
+
         titleAttribute(element, payload) {
           let paragraph = document.createElement('p');
           paragraph.textContent = 'This paragraph has a title.'
           paragraph.title = payload;
           element.insertAdjacentElement('beforeend', paragraph);
         }
-      
+
         linkHref(element, payload) {
           let link = document.createElement('a');
           link.textContent = 'Click here to test your payload as a URL!'
           link.href = payload;
           element.insertAdjacentElement('beforeend', link);
         }
-      
+
         iframeSrc(element, payload) {
           let iframe = document.createElement('iframe');
           iframe.src = payload;
           element.insertAdjacentElement('beforeend', iframe);
         }
-      
+
         trustedStyleBlock(element, payload) {
           let styleBlock = document.createElement('style');
           styleBlock.setAttribute('type', 'text/css');
           styleBlock.textContent = payload;
           element.insertAdjacentElement('beforeend', styleBlock);
         }
-      
+
         trustedStyleAttribute(element, payload) {
           let styledElement = document.createElement('div');
           styledElement.textContent = 'Element with custom style';
           styledElement.setAttribute('style', payload);
           element.insertAdjacentElement('beforeend', styledElement);
         }
-      
+
         trustedScriptBlock(element, payload) {
           let scriptBlock = document.createElement('script');
           scriptBlock.setAttribute('type', 'text/javascript');
           scriptBlock.textContent = '\n' + payload + '\n';
           element.insertAdjacentElement('beforeend', scriptBlock);
         }
-      
+
         trustedScriptBlockPlainMockIframe(element, payload) {
           let iframe = document.createElement('iframe');
           iframe.src = '/assets/mocks/plain.html';
@@ -190,7 +190,7 @@
           });
           element.insertAdjacentElement('beforeend', iframe);
         }
-      
+
         trustedScriptBlockPlainMockWindow(element, payload) {
           let newWindow = window.open('/assets/mocks/plain.html', '_blank');
           newWindow.addEventListener('load', () => {
@@ -203,7 +203,7 @@
             newWindow.document.body.insertAdjacentElement('beforeend', scriptBlock);
           });
         }
-      
+
         trustedScriptBlockStorageMockIframe(element, payload) {
           let iframe = document.createElement('iframe');
           iframe.src = '/assets/mocks/storage.html';
@@ -218,7 +218,7 @@
           });
           element.insertAdjacentElement('beforeend', iframe);
         }
-      
+
         trustedScriptBlockStorageMockWindow(element, payload) {
           let newWindow = window.open('/assets/mocks/storage.html', '_blank');
           newWindow.addEventListener('load', () => {
@@ -231,7 +231,7 @@
             newWindow.document.body.insertAdjacentElement('beforeend', scriptBlock);
           });
         }
-      
+
         trustedScriptBlockCookiesMockIframe(element, payload) {
           let iframe = document.createElement('iframe');
           iframe.src = '/assets/mocks/cookies.html';
@@ -246,7 +246,7 @@
           });
           element.insertAdjacentElement('beforeend', iframe);
         }
-      
+
         trustedScriptBlockCookiesMockWindow(element, payload) {
           let newWindow = window.open('/assets/mocks/cookies.html', '_blank');
           newWindow.addEventListener('load', () => {
@@ -266,72 +266,72 @@
         text(element, payload) {
           $(element).text(payload);
         }
-      
+
         html(element, payload) {
           $(element).html(payload);
         }
-      
+
         jQueryConstructor(element, payload) {
           $(payload).appendTo(element);
         }
-      
+
         prepend(element, payload) {
           $(element)
             .html($('<p>').text('This is a static paragraph. Prepending to its parent element...'))
             .prepend(payload);
         }
-      
+
         append(element, payload) {
           $(element)
             .html($('<p>').text('This is a static paragraph. Appending to its parent element...'))
             .append(payload);
         }
-      
+
         before(element, payload) {
           $('<p>').text('This is a static paragraph. Inserting before it...')
             .prependTo(element)
             .before(payload);
         }
-      
+
         after(element, payload) {
           $('<p>').text('This is a static paragraph. Inserting after it...')
             .appendTo(element)
             .after(payload);
         }
-      
+
         wrapInner(element, payload) {
           $(element)
             .html(
               $('<p>').text('This is a static paragraph. Wrapping around its contents...').wrapInner(payload)
             );
         }
-      
+
         wrap(element, payload) {
           $('<p>').text('This is a static paragraph. Wrapping around all its parent\'s contents...')
             .appendTo(element)
             .wrap(payload);
         }
-      
+
         replaceWith(element, payload) {
           $('<p>').text('This is a static paragraph. Replacing it...')
             .appendTo(element)
             .replaceWith(payload);
         }
-      
+
         titleAttribute(element, payload) {
           $(element)
             .html(
               $('<p>').text('This paragraph has a title.').attr('title', payload)
             );
         }
-      
+
         linkHref(element, payload) {
           $(element)
             .html(
               $('<a>').text('Click here to test your payload as a URL!').attr('href', payload)
             );
         }
-      
+
         iframeSrc(element, payload) {
           $(element)
             .html(
@@ -339,7 +339,7 @@
             );
         }
       }
-    } 
+    }
   }
 
   window.XssDemoApp = window.XssDemoApp || {};
