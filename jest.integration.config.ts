ad5de8a47331e85d24bef533eb2fe797b4eb776f
Michael Riedel <meeque@meeque.de>
2025-07-30 15:12:52 +0200

    Add jest selenium webdriver for integration tests.
    Split jest config for integration tests into a separate file.

--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -4,5 +4,5 @@ import { createCjsPreset } from 'jest-preset-angular/presets';
 export default {
   ...createCjsPreset(),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '[.]integration[.]spec[.]ts$'],
+  testMatch: ['**/*.integration.spec.ts'],
 } satisfies Config;
