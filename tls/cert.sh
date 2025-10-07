c4a3a7589728ac4a58ad05b411d09c4b35f9ffa6
Michael Riedel <meeque@meeque.de>
2025-10-07 16:24:03 +0200

    Tweak certificate file names.
    Also tweak cert helper script log outputs.

--- tls/cert.sh
+++ tls/cert.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo 'XSS Demo: Running certificate helper script...'
+echo 'XSS Demo App: Running certificate helper script...'
 xss_demo_tls_dir="$( realpath "$(dirname "$0")" )"
 cd "${xss_demo_tls_dir}"
 
-echo 'XSS Demo: Setting up "tls/private" dir for private keys...'
-mkdir -p private
-chmod go-rwx private
-
-if [[ -f 'xss-dev.csr.pem' && -f 'xss-dev.cert.pem' && -f 'private/xss-dev.key.pem'  ]]
+if [[ -f 'xss-demo-app.csr.pem' && -f 'xss-demo-app.cert.pem' && -f 'xss-demo-app.key.pem'  ]]
 then
-    echo 'XSS Demo: Private key and certificate for development and testing purposes already exist.'
-    echo 'XSS Demo: Skipping generation of new key pair and certificate.'
-    echo 'XSS Demo: In order to regenerate, delete all "*.pem" files in the "'"${xss_demo_tls_dir}"'" dir and run this script again!'
+    echo 'XSS Demo App App: Private key and certificate for development and testing purposes already exist.'
+    echo 'XSS Demo App: Skipping generation of new key pair and certificate.'
+    echo 'XSS Demo App: In order to regenerate, delete all "*.pem" files in the "'"${xss_demo_tls_dir}"'" dir and run this script again!'
 else
-    echo 'XSS Demo: Generating key pair and certificate for development and testing purposes...'
-    openssl req -new -config 'xss-dev.cert.conf' -noenc -keyout 'private/xss-dev.key.pem' -out 'xss-dev.csr.pem'
-    openssl x509 -req -days 90 -copy_extensions copyall -sha512 -in 'xss-dev.csr.pem' -signkey 'private/xss-dev.key.pem' -out 'xss-dev.cert.pem'
-    echo 'XSS Demo: Successfully generated key pair and certificate for development and testing purposes.'
+    echo 'XSS Demo App: Generating key pair and certificate for development and testing purposes...'
+    openssl req -new -config 'xss-demo-app.cert.conf' -noenc -keyout 'xss-demo-app.key.pem' -out 'xss-demo-app.csr.pem'
+    openssl x509 -req -days 90 -copy_extensions copyall -sha512 -in 'xss-demo-app.csr.pem' -signkey 'xss-demo-app.key.pem' -out 'xss-demo-app.cert.pem'
+    echo 'XSS Demo App: Successfully generated key pair and certificate for development and testing purposes.'
 fi
 
-echo 'XSS Demo: Certificate fingerprints:'
-echo -n 'XSS Demo: '
-openssl x509 -in xss-dev.cert.pem -noout -fingerprint -sha1
-echo -n 'XSS Demo: '
-openssl x509 -in xss-dev.cert.pem -noout -fingerprint -sha256
+echo 'XSS Demo App: Certificate fingerprints:'
+echo -n 'XSS Demo App: '
+openssl x509 -in xss-demo-app.cert.pem -noout -fingerprint -sha1
+echo -n 'XSS Demo App: '
+openssl x509 -in xss-demo-app.cert.pem -noout -fingerprint -sha256
 
-echo 'XSS Demo: Certificate helper script done.'
+echo 'XSS Demo App: Certificate helper script done.'
