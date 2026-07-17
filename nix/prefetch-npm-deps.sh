c98f3150ad1f826ff3bafae0c3cd12c0dd2c22dc
Michael Riedel <meeque@meeque.de>
2026-07-17 13:05:13 +0200

    In Nix config, add error message for missing `npm-deps-hash.nix` file.
    Also make all build error messages more consistent.
    In particular, use backticks to quote files and commands.

--- nix/prefetch-npm-deps.sh
+++ nix/prefetch-npm-deps.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-echo 'XSS Demo App: Running Nix helper script...'
 
-if ! command -v prefetch-npm-deps &> /dev/null
-then
-  echo 'XSS Demo App: Command "prefetch-npm-deps" not found, skipping Nix npmDepsHash update.'
-  exit 0
-fi
+
+echo 'XSS Demo App: Running Nix helper script...'
 
 xss_demo_nix_dir="$( realpath "$(dirname "$0")" )"
 cd "${xss_demo_nix_dir}"
 
+
+
 npm_deps_hash_file='npm-deps-hash.nix'
 package_lock_file='../package-lock.json'
 
-npm_deps_hash="$(prefetch-npm-deps "${package_lock_file}")"
-echo "\"${npm_deps_hash}\"" > "${npm_deps_hash_file}"
+if command -v prefetch-npm-deps &> /dev/null
+then
+  echo "XSS Demo App: Updating file \`${npm_deps_hash_file}\` with npmDepsHash..."
 
-echo "XSS Demo App: Updated file \"${npm_deps_hash_file}\" with npmDepsHash: \"${npm_deps_hash}\"."
+  npm_deps_hash="$(prefetch-npm-deps "${package_lock_file}")"
+  echo "\"${npm_deps_hash}\"" > "${npm_deps_hash_file}"
+
+  echo "XSS Demo App: Updated file \`${npm_deps_hash_file}\` with npmDepsHash: \`${npm_deps_hash}\`."
+else
+  echo "XSS Demo App: Command \`prefetch-npm-deps\` not found on \`PATH\`."
+  echo "XSS Demo App: Skipping update of file \`${npm_deps_hash_file}\` with npmDepsHash."
+fi
+
+
 
 echo 'XSS Demo App: Nix helper script done.'
--- nix/xss-demo-app.nix
+++ nix/xss-demo-app.nix
@@ -29,7 +29,28 @@ in
         );
     };
 
-    npmDepsHash = import ./npm-deps-hash.nix;
+    npmDepsHash =
+      if ( builtins.pathExists ./npm-deps-hash.nix )
+      then (
+        import ./npm-deps-hash.nix
+      )
+      else (
+        builtins.throw ''
+          File `nix/npm-deps-hash.nix` is missing!
+          This file must be present and contain a hash based on the contents of `package-lock.json`.
+          You can fix this error in several ways:
+
+          - If you have `prefetch-npm-deps` on the `PATH`:
+            Generate the file by running `npm install` **or** `nix/prefetch-npm-deps.sh`.
+            (Hint: You can put `prefetch-npm-deps` on the `PATH` by running: `nix-env --install prefetch-npm-deps`.)
+
+          - Otherwise:
+            Create file `nix/npm-deps-hash.nix` that contains only an empty Nix string (`""`).
+            Next run `nix-build`, which will eventually throw an error and print the expected hash.
+            Finally, edit `nix/npm-deps-hash.nix`, so it contains a Nix string that holds the expected hash.
+
+        ''
+      );
 
     npmFlags = [ "--ignore-scripts" ];
 
